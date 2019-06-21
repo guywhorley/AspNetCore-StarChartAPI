f7e9af3f69e0d1c05b3107d0e13413352d7e2aa9
Guy Whorley <guy.whorley@gmail.com>
2019-06-21 14:58:22 -0700

    Finished adding properties to celestial object.

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        /// <summary>
+        /// Id property.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of celestial object.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+        /// <summary>
+        /// Id of orbited object
+        /// </summary>
+        public int? OrbitedObjectId { get; set; }
+        /// <summary>
+        /// List of celestial object satellites.
+        /// </summary>
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+        /// <summary>
+        /// Timespan for orbital period.
+        /// </summary>
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
